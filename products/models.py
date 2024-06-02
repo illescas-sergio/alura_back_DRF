a482cdd5b6fcca88b063d7f551f65f70341a1fc1
Sergio <illescas.s.m@gmail.com>
2024-06-02 12:03:12 -0300

    images ok

--- products/models.py
+++ products/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     product_description = models.CharField(max_length=250)
     category = models.CharField(max_length=50, blank=True)
     price = models.FloatField()
-    product_image = models.ImageField(blank=True, null=True)
+    product_image = models.ImageField(blank=True, null=True, upload_to='productos/')
     product_owner = models.ForeignKey(to=get_user_model(), related_name = 'published_products', on_delete=models.CASCADE)
 
 
